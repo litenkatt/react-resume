d3fbe23f00a0402a545db776df83ab26976e2914
Ninni Hörnaeus <litenkattunge@gmail.com>
2020-08-27 21:36:10 +0200

    added language path option

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,19 @@
 import i18n from 'i18next';
+import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 
+const options = {
+  order: ['path', 'navigator']
+}
+
 i18n
+  .use(detector)
   .use(Backend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    lng: 'en',
+    detection: options,
     fallbackLng: 'en',
-
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
